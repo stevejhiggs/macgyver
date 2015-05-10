77425969c1a92d112b51dbb74d19cb2967757226
Steve Higgs <shiggs@opentable.com>
2015-05-10 16:15:30 +0100

    pull out web pack config into its own config

--- reactIsoFluxible/Gruntfile.js
+++ reactIsoFluxible/Gruntfile.js
@@ -1,8 +1,6 @@
-var webpack = require('webpack');
-
 module.exports = function (grunt) {
     grunt.initConfig({
-        clean: ['./build'],
+        clean: ['./public/generated'],
         concurrent: {
             dev: ['nodemon:dev', 'webpack:dev'],
             options: {
@@ -19,33 +17,7 @@ module.exports = function (grunt) {
             }
         },
         webpack: {
-            dev: {
-                resolve: {
-                    extensions: ['', '.js', '.jsx']
-                },
-                entry: './src/client.js',
-                output: {
-                    path: './public/generated/js',
-                    filename: 'client.js'
-                },
-                module: {
-                    loaders: [
-                        { test: /\.css$/, loader: 'style!css' },
-                        { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: require.resolve('babel-loader') }
-                    ]
-                },
-                plugins: [
-                    // Protects against multiple React installs when npm linking
-                    new webpack.NormalModuleReplacementPlugin(/^react?$/, require.resolve('react')),
-                    new webpack.NormalModuleReplacementPlugin(/^react(\/addons)?$/, require.resolve('react/addons'))
-                ],
-                stats: {
-                    colors: true
-                },
-                devtool: 'source-map',
-                watch: true,
-                keepalive: true
-            }
+            dev: require("./config/webpack.config.dev.js")
         }
     });
 
---
+++ reactIsoFluxible/config/webpack.config.dev.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    entry: './src/client.js',
+    output: {
+        path: './public/generated/js',
+        filename: 'client.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: require.resolve('babel-loader') }
+        ]
+    },
+    plugins: [
+        // Protects against multiple React installs when npm linking
+        new webpack.NormalModuleReplacementPlugin(/^react?$/, require.resolve('react')),
+        new webpack.NormalModuleReplacementPlugin(/^react(\/addons)?$/, require.resolve('react/addons'))
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    watch: true,
+    keepalive: true
+};
