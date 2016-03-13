02cb4eafaeff1df059abbe0855da10d42c4d3ad5
Steve Higgs <steve.higgs@salecycle.com>
2016-03-13 16:57:40 +0000

    make route structure match the structure used in the universal example

--- clientSideOnly/redux/react-router/src/index.js
+++ clientSideOnly/redux/react-router/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router';
-import Routes from './routes';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import routes from './routes';
 import configureStore from './configureStore';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Routes history={browserHistory} store={store} />
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('root')
 );
--- clientSideOnly/redux/twitterBootstrap/src/routes.jsx
+++ clientSideOnly/redux/twitterBootstrap/src/routes.jsx
@@ -1,33 +1,16 @@
-import React, { PropTypes } from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
 import LayoutView from './views/layout';
 import HomeView from './views/home';
 import AboutView from './views/about';
 import NotFoundView from './views/notFound';
 
-const routes = (props) => {
-  /* history preserves browser history so things like the back button work */
-  const { history, store } = props;
-
-  // Create an enhanced history that syncs navigation events with the store
-  const enhancedHistory = syncHistoryWithStore(history, store);
-
-  return (
-    <Router history={enhancedHistory}>
-      <Route path="/" component={LayoutView}>
-        <IndexRoute component={HomeView} />
-        <Route path="about" component={AboutView} />
-        <Route path="*" component={NotFoundView} />
-      </Route>
-    </Router>
-  );
-};
-
-routes.propTypes = {
-  history: PropTypes.object,
-  store: PropTypes.object
-};
-
+const routes = (
+  <Route path="/" component={LayoutView}>
+    <IndexRoute component={HomeView} />
+    <Route path="about" component={AboutView} />
+    <Route path="*" component={NotFoundView} />
+  </Route>
+);
 
 export default routes;
