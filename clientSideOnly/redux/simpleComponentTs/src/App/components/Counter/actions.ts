e2da24db2d047410ab5c36a1ef0906a5e3b21187
Steve Higgs <stevejhiggs@hotmail.com>
2017-06-28 21:25:29 +0100

    remove uneeded types

--- clientSideOnly/redux/simpleComponentTs/src/App/components/Counter/actions.ts
+++ clientSideOnly/redux/simpleComponentTs/src/App/components/Counter/actions.ts
@@ -2,7 +2,7 @@ import actionCreatorFactory from 'typescript-fsa';
 
 const actionCreator = actionCreatorFactory();
 
-export const decrement = actionCreator<{}>('DECREMENT_COUNTER');
-export const increment = actionCreator<{}>('INCREMENT_COUNTER');
+export const decrement = actionCreator('DECREMENT_COUNTER');
+export const increment = actionCreator('INCREMENT_COUNTER');
 
 
