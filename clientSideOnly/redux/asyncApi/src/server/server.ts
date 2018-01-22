968fc38c94d1fe3cde3a5fc15464b398798c7f37
Steve Higgs <steve.higgs@salecycle.com>
2018-01-22 10:32:59 +0000

    transparently integrate the api server

--- clientSideOnly/redux/asyncApi/src/server/server.ts
+++ clientSideOnly/redux/asyncApi/src/server/server.ts
@@ -1,19 +1,22 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser')
-const cors = require('koa2-cors');
-const Router = require('koa-router');
+import * as Koa from 'koa';
+import * as bodyParser from 'koa-bodyparser';
+import * as Router from 'koa-router';
+import * as cors from 'koa2-cors';
+import * as staticFiles from 'koa-static';
+import * as path from 'path';
 
 function createServer() {
   const app = new Koa();
   app.use(bodyParser());
   app.use(cors());
+  app.use(staticFiles(path.join(__dirname, 'public')));
 
   const router = new Router();
   router.get('/api/animals', async (ctx) => {
     ctx.body =  [
-      {id:1, name: 'cat'},
-      {id:2, name: 'dog'},
-      {id:3, name: 'fish'}
+      {id: 1, name: 'cat'},
+      {id: 2, name: 'dog'},
+      {id: 3, name: 'fish'}
     ];
   });
 
@@ -43,9 +46,10 @@ function createServer() {
 const server = createServer();
 try {
   const listener = server.listen(8000, () => {
+    // tslint:disable-next-line:no-console
     console.log(`Server started on port ${listener.address().port}`);
   });
 } catch (e) {
+  // tslint:disable-next-line:no-console
   console.error(e);
 }
-
