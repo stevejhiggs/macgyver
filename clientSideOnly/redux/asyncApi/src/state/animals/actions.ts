4ba972e966786a1d349ae9ee7b13e87be4402ddb
Steve Higgs <stevejhiggs@hotmail.com>
2018-01-28 20:40:42 +0000

    undo accidental name change

--- clientSideOnly/redux/asyncApi/src/state/animals/actions.ts
+++ clientSideOnly/redux/asyncApi/src/state/animals/actions.ts
@@ -6,7 +6,7 @@ export interface LoadAnimalsParams {
   name: string;
 }
 
-export const loadAnimals = promiseAction('ANIMALS2_LOADING', async (params: LoadAnimalsParams): Promise<Animal[]> => {
+export const loadAnimals = promiseAction('ANIMALS_LOADING', async (params: LoadAnimalsParams): Promise<Animal[]> => {
   const res = await fetch('/api/animals');
   if (res.status >= 400) {
     throw new Error(`Server error: ${res.status} ${res.statusText}`);
