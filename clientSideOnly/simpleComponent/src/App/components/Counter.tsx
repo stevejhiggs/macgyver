874614b731937dbc5d8c13827ac544a51f29a605
steve higgs <steve.x.higgs@gsk.com>
2019-04-13 11:14:14 +0100

    We can also do function binding easily with hooks

--- clientSideOnly/simpleComponent/src/App/components/Counter.tsx
+++ clientSideOnly/simpleComponent/src/App/components/Counter.tsx
@@ -13,10 +13,19 @@ const Counter: FunctionComponent<Props> = ({ startVal = 0 }) => {
   // setState is a function that accepts either a number or a function returning
   // a number
   const [state, setState] = useState<State>({count: startVal});
+
+  function increment(by: number) {
+    setState({ count: state.count + by });
+  }
+
+  function decrement(by: number) {
+    setState({ count: state.count - by });
+  }
+  
   return <>
     <p>Clicked: {state.count} times</p>
-    <button onClick={() => setState({ count: state.count + 1})}>+</button>
-    <button onClick={() => setState({ count: state.count - 1})}>-</button>
+    <button onClick={() => increment(1)}>+</button>
+    <button onClick={() => decrement(1)}>-</button>
   </>
 }
 
