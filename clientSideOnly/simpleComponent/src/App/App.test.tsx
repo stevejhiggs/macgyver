03dbabc3a785c608e8279143143bcc9e3cd5eec0
Steve Higgs <steve@veed.io>
2022-09-20 20:00:50 +0100

    remove uneeded createElement

--- clientSideOnly/simpleComponent/src/App/App.test.tsx
+++ clientSideOnly/simpleComponent/src/App/App.test.tsx
@@ -2,6 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./index";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
   createRoot(document.createElement("div")).render(<App />);
 });
